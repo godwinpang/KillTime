319b019ad3d8aae3cb14f65093709dc4a05777df
Guanxin <liguanxin078@gmail.com>
2018-02-04 05:34:56 -0800

    added QTimeCode Feature

--- Website/index.js
+++ Website/index.js
@@ -34,8 +34,33 @@ var endLocation = document.getElementById("endLocation");
 var endTimeElem = document.getElementById("endTime");
 var submitButton = document.getElementById("submitButton");
 var keywordsArea = document.getElementById("keywords");
-
+var QTimeCode = document.getElementById("QTimeCode");
 var paramsValid = false;
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 // Updates the global variables with the data inputted by the user
 function retrieveParams() {
